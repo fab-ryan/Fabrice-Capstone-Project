077fd2877ec81c517e2a08e6b3519c317ee07ccd
fab-ryan <royalfabrice1234@gmail>
2022-02-22 16:03:06 +0200

    firebase

--- js/app.js
+++ js/app.js
@@ -1,22 +1,38 @@
-
-const navMenu = document.getElementById('nav-menu'),
-      toggleMenu = document.getElementById('nav-toggle'),
-      closeMenu = document.getElementById('nav-close')
-
-
-toggleMenu.addEventListener('click', ()=>{
-    navMenu.classList.toggle('show')
-})
-
-
-closeMenu.addEventListener('click', ()=>{
-    navMenu.classList.remove('show')
-})
-const navLink = document.querySelectorAll('.nav__link');   
-function linkAction(){
+const navMenu = document.getElementById("nav-menu"),
+  toggleMenu = document.getElementById("nav-toggle"),
+  closeMenu = document.getElementById("nav-close"),
+  Main = document.getElementById("main");
  
-  navLink.forEach(n => n.classList.remove('active'));
-  this.classList.add('active');
-   navMenu.classList.remove('show')
+
+toggleMenu.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+  Main.classList.toggle("blure");
+});
+
+closeMenu.addEventListener("click", () => {
+  navMenu.classList.remove("show");
+  Main.classList.remove("blure");
+});
+const navLink = document.querySelectorAll(".nav__link");
+function linkAction() {
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+  navMenu.classList.remove("show");
+  Main.classList.remove("blure");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+
+
+// firebase configurations
+const firebaseConfig = {
+  apiKey: "AIzaSyBvQqfAlXssffV1ywtxgdYS67i3tB0WuyA",
+  authDomain: "fab-web-site.firebaseapp.com",
+  projectId: "fab-web-site",
+  storageBucket: "fab-web-site.appspot.com",
+  messagingSenderId: "283650575875",
+  appId: "1:283650575875:web:2bd8aed1b6f44ef32f8836"
+};
+ 
+
+ 
